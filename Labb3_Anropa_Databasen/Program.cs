99e4bb424b33bc81e6e578e08a40e923a2a81eef
onni82 <onni.bucht@me.com>
2024-12-11 16:38:55 +0100

    Add project files.

---
+++ Labb3_Anropa_Databasen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb3_Anropa_Databasen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

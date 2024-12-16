6aa0b4cad9d359f73b5c8f96e7bb65f4066d89c0
onni82 <onni.bucht@me.com>
2024-12-16 12:13:50 +0100

    Removed comments

--- Labb3_Anropa_Databasen/Program.cs
+++ Labb3_Anropa_Databasen/Program.cs
@@ -12,5 +12,3 @@ namespace Labb3_Anropa_Databasen
         }
     }
 }
-// Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
-// Scaffold-DbContext "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -ContextDir Data -Context SchoolContext
